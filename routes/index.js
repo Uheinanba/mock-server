a77f20a7346f523236ede4beed89b27f482fcf76
yuank <yuank@fxiaoke.com>
2017-12-08 18:39:11 +0800

    add 前端 admin

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,15 @@
 var express = require('express');
 var router = express.Router();
+var Mock = require('mockjs');
+
+var data = Mock.mock({
+  'list|1-10': [
+    {
+      'id|+1': 1,
+    },
+  ],
+});
+console.log(JSON.stringify(data, null, 4));
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
