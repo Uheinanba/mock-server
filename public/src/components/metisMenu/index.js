ef2602d0ca8a4af29fa6da5d3eccef2db8a3d3f4
heimanba <yk@lingdong365.com>
2017-12-09 18:37:46 +0800

    UI构建工具初步完成

---
+++ public/src/components/metisMenu/index.js
@@ -0,0 +1,4 @@
+import metisMenu from 'metisMenu/src/metisMenu';
+import 'metisMenu/src/metisMenu.css';
+
+export default metisMenu;
---
+++ public/src/index.js
@@ -0,0 +1,4 @@
+import metisMenu from './components/metisMenu';
+
+$('#side-menu').metisMenu();
+console.log(222, metisMenu);
